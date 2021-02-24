5b9e8f0899822bd39687a5812ce74a498236c040
olvanotpoint@gmail.com <0lvanotpoint@gmail.com>
2021-02-24 21:51:48 +0300

    social media updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -11,13 +11,17 @@
 
         @if($socials)
         <ul class="socials">
-              <li class="socials__item"><a href="{{ $socials->facebook }}" target="_blank"><i class="fa fa-facebook"></i> Facebook</a></li>
-              <li class="socials__item"><a href="{{ $socials->twitter }}" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
-              <li class="socials__item"><a href="{{ $socials->youtube }}" target="_blank"><i class="fa fa-youtube"></i> Youtube</a></li>
-              <li class="socials__item"><a href="{{ $socials->instagram }}" target="_blank"><i class="fa fa-instagram"></i> Instagram</a></li>
+          <li class="socials__item"><a href="{{ $socials->facebook }}" target="_blank"><i
+                class="mr-1 fa fa-facebook"></i> Facebook</a></li>
+          <li class="socials__item"><a href="{{ $socials->twitter }}" target="_blank"><i class="mr-1 fa fa-twitter"></i>
+              Twitter</a></li>
+          <li class="socials__item"><a href="{{ $socials->youtube }}" target="_blank"><i class="mr-1 fa fa-youtube"></i>
+              Youtube</a></li>
+          <li class="socials__item"><a href="{{ $socials->instagram }}" target="_blank"><i
+                class="mr-1 fa fa-instagram"></i> Instagram</a></li>
         </ul>
         @endif
-        
+
       </div>
 
 
@@ -28,7 +32,7 @@
           <h2 class="sec-title__title">Galerie Images</h2><span class="sec-title__divider"></span>
         </div><!-- End / sec-title -->
 
-        
+
 
         <!-- widget-gallery -->
 
@@ -36,12 +40,13 @@
           @if($footer_galleries)
           @foreach($footer_galleries as $gallery)
           <div class="widget-gallery__item">
-            <a title="{{ $gallery->title }}" href="{{ getImage($gallery->image) }}"><img src="{{ getImage($gallery->image) }}" alt="{{ $gallery->title }}"/></a>
+            <a title="{{ $gallery->title }}" href="{{ getImage($gallery->image) }}"><img
+                src="{{ getImage($gallery->image) }}" alt="{{ $gallery->title }}" /></a>
           </div>
           @endforeach
           @endif
         </div><!-- End /  widget-gallery -->
-        
+
 
       </div>
 
@@ -61,8 +66,9 @@
         </p>
         <p>
           <a class="quick-link" href="{{ route('page.actuality') }}"><i class="fa fa-angle-right"></i>
-           Actualités</a></p>
-        <p><a class="quick-link" href="{{ route('page.activity') }}"> <i class="fa fa-angle-right"></i> Activités</a></p>
+            Actualités</a></p>
+        <p><a class="quick-link" href="{{ route('page.activity') }}"> <i class="fa fa-angle-right"></i> Activités</a>
+        </p>
         <p><a class="quick-link" href="/#contact"><i class="fa fa-angle-right"></i> Contacts</a></p>
 
       </div>
